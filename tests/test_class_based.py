9b905baebf8af54fe3322f3fd8c6cc8dc7904e12
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2022-01-02 14:35:05 +0300

    test_class_based: add class based fixture test

--- tests/test_class_based.py
+++ tests/test_class_based.py
@@ -13,3 +13,24 @@ def test_class_based_tests(testdir):
     result = testdir.runpytest()
 
     result.assert_outcomes(passed=1)
+
+
+def test_class_based_tests_with_fixture(testdir):
+    testdir.makepyfile("""
+        import pytest
+
+
+        class TestSuite:
+            @pytest.fixture
+            async def test_fixture(self):
+                return "test_fixture"
+
+            @pytest.mark.asyncio_cooperative
+            async def test_cooperative(self, test_fixture):
+                assert test_fixture == "test_fixture"
+
+    """)
+
+    result = testdir.runpytest()
+
+    result.assert_outcomes(passed=1)
